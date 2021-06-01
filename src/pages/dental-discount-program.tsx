57aadaec66c11416d562e1e1c9fafcd53428b021
jomar357 <jomarbruto@gmail.com>
2021-06-01 23:02:59 +0800

    Dental Discount Program Page

--- src/pages/dental-discount-program.tsx
+++ src/pages/dental-discount-program.tsx
@@ -24,6 +24,9 @@ const DentalDiscountProgramPage: React.FC<Props> = ({ data, location }: Props) =
   const posts = data.remark.posts
   const hero = data.hero?.childImageSharp?.fluid
   const hero_background = data.hero_background?.childImageSharp?.fluid
+  const care_pos_logo = data.care_pos_logo?.childImageSharp?.fluid
+  const unoa_logo = data.unoa_logo?.childImageSharp?.fluid
+  const eyemed_logo = data.eyemed_logo?.childImageSharp?.fluid
 
   return (
     <Layout location={location}>
@@ -38,13 +41,180 @@ const DentalDiscountProgramPage: React.FC<Props> = ({ data, location }: Props) =
           <Container>
             <div className="col-lg-12">
               <h1 className="text-white text-center">
-                Dental Discount Program
+                INF DENTAL DISCOUNT PROGRAM
               </h1>
               <button className="hero-btn"> Get a Quote </button>
             </div>
           </Container>
         </div>
       </BackgroundImage>
+
+      <section id="dental-discount-section-1">
+       <Container>
+        <div className="col-lg-12">
+          <div className="dental-discount-container">
+           <h2 className="text-blue text-center">Careington Non-Insurance Discount Program when traveling to the USA.</h2>
+           <p className="text-center">
+            Membership in INF includes access to a Careington discount program at no additional cost, Careington coordinates this discount program, which provides savings on Dental, Vision, Prescription, & Hearing products & services while in the USA. Services are not available in Canada or Mexico. This Discount card program is not an insurance product and is not insured by Crum & Forster SPC. This is not insurance.
+            Way too often someone will call the INF office and say, "I have a toothache, if I go to the dentist will this be covered?".
+            </p>
+            <p className="text-center">
+              All INF plans come with the Careington POS Dental Discount Card for INF members at no cost additional to you. Keep in mind- this is not insurance but a discount card to help with the cost of dental procedures while in the United States. The Careington Dental card is simple to use. When you enroll in an INF plan- the discount card is provided in your welcome email. If you have any dental issue- simply locate one of Careington's 13,000 in-network dentists near you, and visit the dental office. The Careington Dental Plan will act as an instant savings card- affording you dental discounts on services.
+            </p>
+            <p className="text-center">
+              Save 20% to 50% on most dental procedures including routine oral exams, unlimited cleanings, and major work such as dentures, root canals, and crowns through one of the largest dental networks nationally with a focus on neighborhood dentists.
+            </p>
+            <p className="text-center">
+              Cosmetic dentistry such as bonding and veneers also included.
+            </p>
+            <p className="text-center">
+              All dentists must meet highly selective credentialing standards based on education, background, license standing and other requirements.
+            </p>
+            <p className="text-center">
+              You may visit any participating dentist on the plan and change providers at any time.
+            </p>
+            <p className="text-center">
+              To locate a participating provider in the United States, please call 408-540-3601 or visit our INF Provider Search ink to access our online provider search.
+            </p>
+           </div>
+         </div>
+         <div className="col-lg-12">
+         </div>
+      </Container>
+     </section>
+
+     <section id="dental-discount-section-2">
+       <Container>
+         <div className="col-lg-12">
+           <p className="text-black text-center">The services described in this article are not Insurance and are not affiliated with Crum & Forster SPC. This is not an Insurance product.</p>
+           <h2 className="text-blue text-center">Careington Dental Discount FAQ:</h2>
+           <div className="dental-discount-faq1-tab">
+             <input checked="checked" id="dental-discount-faq1-tab1" type="radio" name="dental-discount-faq1" />
+             <input id="dental-discount-faq1-tab2" type="radio" name="dental-discount-faq1" />
+             <nav>
+               <ul>
+                 <li className="dental-discount-faq1-tab1">
+                   <label for="dental-discount-faq1-tab1">How are claims handled by the Careington Dental Discount Plan?</label>
+                 </li>
+                 <li className="dental-discount-faq1-tab2">
+                   <label for="dental-discount-faq1-tab2">Why is it important to have access to dental discounts when visiting the United States?</label>
+                 </li>
+               </ul>
+             </nav>
+             <section>
+               <div className="dental-discount-faq1-tab1">
+                 <p className="text-black">The beauty of the Careington Dental Discount Plan is that there is no hassle of filing claims or filing for reimbursement from the company. Rather- the Careington card acts as a discount card or instant savings card, meaning you pay the discounted rate at participating dentists.</p>
+                 <p className="text-black">For example- you visit a dental office that charges $600 for a root canal that you desperately need. If the dental office participates in the Careington Dental Discount Plan all you need to do is show your Careington Dental Discount card to the front receptionist, you will then get a discounted rate for your procedure. The discounted rate will be the price you pay. Very simple- no need to file for claims or reimbursement- depending on the procedure you can save as much as over 50% of the cost by just showing your card!</p>
+               </div>
+               <div className="dental-discount-faq1-tab2">
+                 <p className="text-black">​Many visitors run into some sort of dental issue while outside their home country. In fact, a survey of INF Members in 2018 found that 46% of insured persons who visited the US complained of some sort of dental issue during their visit. A common question we're asked at INF is, "I can't eat anything due to this toothache! What can I do?" According to MemberBenefits.com, the average cost of a regular dental visit is going to be almost $285- let alone the cost of a tooth extraction or new dentures!</p>
+               </div>
+             </section>
+           </div>
+         </div>
+       </Container>
+
+       <Container>
+         <div className="col-lg-12">
+           <div className="dental-discount-faq2-tab">
+             <input checked="checked" id="dental-discount-faq2-tab1" type="radio" name="dental-discount-faq2" />
+             <input id="dental-discount-faq2-tab2" type="radio" name="dental-discount-faq2" />
+             <input id="dental-discount-faq2-tab3" type="radio" name="dental-discount-faq2" />
+             <input id="dental-discount-faq2-tab4" type="radio" name="dental-discount-faq2" />
+             <nav>
+               <ul>
+                 <li className="dental-discount-faq2-tab1">
+                   <label for="dental-discount-faq2-tab1">Careington POS Dental Discount</label>
+                 </li>
+                 <li className="dental-discount-faq2-tab2">
+                   <label for="dental-discount-faq2-tab2">UNoA Prescription Discount Program</label>
+                 </li>
+                 <li className="dental-discount-faq2-tab3">
+                   <label for="dental-discount-faq2-tab3">EyeMed Discount Program</label>
+                 </li>
+                 <li className="dental-discount-faq2-tab4">
+                   <label for="dental-discount-faq2-tab4">Amplifon Hearing Discount Program</label>
+                 </li>
+               </ul>
+             </nav>
+             <section>
+               <div className="dental-discount-faq2-tab1">
+                 <div className="row">
+                  <div className="col-lg-8">
+                    <p className="text-left text-black tab-title">Careington POS Dental Discount</p>
+                    <ul>
+                      <li className="text-left text-black">Save 20% to 50% on most dental procedures including routine oral exams, unlimited cleanings, and major work such as dentures, root canals, and crowns through one of the largest dental networks nationally with a focus on neighborhood dentists.</li>
+                      <li className="text-left text-black">Cosmetic dentistry such as bonding and veneers also included</li>
+                      <li className="text-left text-black">All dentists must meet highly selective credentialing standards based on education, background, license standing and other requirements</li>
+                    </ul>
+                  </div>
+                  <div className="col-lg-4">
+                    <Img fluid={care_pos_logo} className="right-logo" />
+                  </div>
+                  <div className="col-lg-12 bottom-p">
+                    <ul>
+                      <li className="text-left text-black">You may visit any participating dentist on the plan and change providers at any time</li>
+                      <li className="text-left text-black">To locate a participating provider, please call <strong>408-540-3601</strong> or visit our <Link to="/">INF Provider Search</Link> link to access our online provider search.</li>
+                    </ul>
+                  </div>
+                 </div>
+               </div>
+               <div className="dental-discount-faq2-tab2">
+                 <div className="row">
+                  <div className="col-lg-7">
+                    <p className="text-left text-black tab-title">UNoA Prescription Discount Program</p>
+                    <ul>
+                      <li className="text-left text-black">Members will have access to savings between 15% to 60% off the retail price of generic drugs and 10% to 25% off the retail price of brand name drugs at over 68,000 participating pharmacies nationwide including Safeway, CVS, Duane Reade, Wal-Mart, Target, Walgreens, Rite Aid and many more.</li>
+                    </ul>
+                  </div>
+                  <div className="col-lg-5">
+                    <Img fluid={unoa_logo} className="right-logo" />
+                  </div>
+                  <div className="col-lg-12 bottom-p">
+                    <ul>
+                      <li className="text-left text-black">At the pharmacy, members will simply present their membership card to save on prescription drugs. Even if members have prescription benefits through a health insurance carrier, a comparison of costs between the two programs should be performed to determine the most savings.</li>
+                    </ul>
+                  </div>
+                 </div>
+               </div>
+               <div className="dental-discount-faq2-tab3">
+                 <div className="row">
+                  <div className="col-lg-7">
+                    <p className="text-left text-black tab-title">EyeMed Discount Program </p>
+                    <ul>
+                      <li className="text-left text-black">Regular eye exams are not good for your eye health – they’re good for your overall health, too. Annual eye exams can detect signs of serious health issues, such as diabetes and high blood pressure.</li>
+                    </ul>
+                  </div>
+                  <div className="col-lg-5">
+                    <Img fluid={eyemed_logo} className="right-logo" />
+                  </div>
+                  <div className="col-lg-12 bottom-p">
+                    <ul>
+                      <li className="text-left text-black">Members save 20% to 40% off the retail price of eyewear with the EyeMed Vision Care Access Plan D discount program through the Access network. Members are eligible for discounts on exams, eyeglasses and contact lenses from more than 90,000 providers nationwide including independent optometrists, ophthalmologists, opticians and leading optical retailers such as LensCrafters®, Sears Optical®, Target Optical®, JCPenney® Optical and most Pearle Vision® locations.</li>
+                      <li className="text-left text-black">Members can choose the doctors they want and the brands they love, like Ray-Ban®, Oakley ® and Coach ®.</li>
+                    </ul>
+                  </div>
+                 </div>
+               </div>
+               <div className="dental-discount-faq2-tab4">
+                <div className="row">
+                 <div className="col-lg-12">
+                   <p className="text-left text-black tab-title">Amplifon Hearing Discount Program</p>
+                   <ul>
+                     <li className="text-left text-black">Hearing aid low price guarantee: If you should find the same product at a lower price, bring us the local quote and we’ll not match it, we’ll beat it by 5%!</li>
+                     <li className="text-left text-black">As a member, you will receive a <strong>40% discount</strong> on diagnostic services, including hearing exams.</li>
+                     <li className="text-left text-black">2 years of free batteries (80 cells per hearing aid, per year)</li>
+                     <li className="text-left text-black"><strong>For more information or to schedule an appointment</strong>, call toll-free <strong>1-866-211-6045</strong>. Our Patient Care Advocate will explain our program, answer any questions or schedule an appointment at any of our over 3,800 locations nationwide.</li>
+                     <li className="text-left text-black">Learn more about our program at <a href="www.amplifonusa.com/careington" target="_blank">www.amplifonusa.com/careington</a>.</li>
+                   </ul>
+                 </div>
+                </div>
+               </div>
+             </section>
+           </div>
+         </div>
+       </Container>
+     </section>
     </Layout>
   )
 }
@@ -89,9 +259,30 @@ export const query = graphql`
         }
       }
     },
-    hero_background: file(name: { eq: "contact-hero-bg" }) {
+    hero_background: file(name: { eq: "dental-discount-hero-bg" }) {
       childImageSharp {
         fluid(maxWidth: 1400, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    },
+    care_pos_logo: file(name: { eq: "care-pos-logo" }) {
+      childImageSharp {
+        fluid(maxWidth: 500, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    },
+    unoa_logo: file(name: { eq: "UNOA-logo" }) {
+      childImageSharp {
+        fluid(maxWidth: 500, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    },
+    eyemed_logo: file(name: { eq: "eyemed-logo" }) {
+      childImageSharp {
+        fluid(maxWidth: 500, quality: 100) {
           ...GatsbyImageSharpFluid
         }
       }
