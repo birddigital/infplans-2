41cfc51daa51e8ad64bafdddb570ba143adffac7
23Moo23 <victormercado031@gmail.com>
2021-06-05 02:08:11 +0800

    about why inf

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -18,37 +18,50 @@ const AboutPage: React.FC<Props> = ({ data, location }: Props) => {
   const meta = data.site?.meta
   const hero = data.hero?.childImageSharp?.fluid
   const hero_background = data.hero_background?.childImageSharp?.fluid
+  const infplans_big_logo = data.infplans_big_logo?.childImageSharp?.fluid
+
 
   return (
     <Layout location={location}>
       <Meta site={meta} title="About Us - INF Visitor Insurance" />
       <BackgroundImage
         Tag="section"
-        className="hero-section-bg"
+        className="about-hero-section-bg"
         fluid={hero_background}
         backgroundColor={`#e9f4f8`}
-        alt=""
+        alt="about hero background"
       >
-        <div id="hero-section">
+        <div id="about-hero-section">
           <Container>
             <div className="col-lg-12">
-              <h1 className="text-black">
-                Your Title Here
+              <h1 className="text-white text-center">
+                ABOUT US
               </h1>
-              <p className="hero-text text-black">
-                subtitle here
-              </p>
-              <ButtonBlack path="/contact" label="Button Text" />
-            </div>
-            <div className="col-lg-8">
+              <button className="hero-btn"> Get a Quote </button>
             </div>
           </Container>
         </div>
       </BackgroundImage>
-      <div id="your-id-here">
+
+      <section id="about-section-1">
         <Container>
+          <div className="col-lg-12">
+            <div className="about-container">
+              <h2 className="text-blue text-center">Who is INF?</h2>
+              <Img fluid={infplans_big_logo} className="big-logo"/>
+              <p className="text-blue text-center sub-title">"At the core of INF is a community organization..."</p>
+              <p>INF (the India Network Foundation) is an organization that provides innovative international visitor accident and sickness insurance options for international travelers as well as various non insurance benefits.We provide international visitor accident & sickness insurance for visiting parents, relatives, students, and temporary workers traveling outside of their home country to the United States, Canada or Mexico. INF provides some plan options which can cover pre-existing conditions, meaning we may cover any pre-existing condition up to the stated limits.</p>
+              <p>As a member of INF, you are able to access international visitor accident & sickness insurance, find coverage options for pre-existing medical conditions, and help ease the possible financial burden due to unforeseen medical expenses during your stay in the United States, Canada, or Mexico.</p>
+              <p>INF IVAS plans can provide coverage for pre-existing conditions, as defined in the applicable insurance policies.</p>
+              <p>As a community organization, we are able to offer insurance programs for non-US citizens visiting the United States, Canada, or Mexico.</p>
+              <p>All INF IVAS plans use Multiplan Network.</p>
+              <p>Most INF International Visitor Accident and Sickness plans provide coverage for pre-existing conditions. Our A-rated underwriting partners provide the strong financial support needed from an insurance company to continue offering International Visitor Accident and Sickness plans with pre-existing conditions coverage.</p>
+              <p>If you have any questions about INF or our organization, please email us at support[at]infplans.com.</p>
+            </div>
+          </div>
         </Container>
-      </div>
+      </section>
+
     </Layout>
   )
 }
@@ -64,9 +77,16 @@ export const query = graphql`
         siteUrl
       }
     },
-    hero_background: file(name: { eq: "bg" }) {
+    hero_background: file(name: { eq: "request-a-quote-hero-bg" }) {
       childImageSharp {
         fluid(maxWidth: 1400, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    },
+    infplans_big_logo: file(name: { eq: "infplans-big-logo" }) {
+      childImageSharp {
+        fluid(maxWidth: 600, quality: 100) {
           ...GatsbyImageSharpFluid
         }
       }
--- src/pages/why-inf.tsx
+++ src/pages/why-inf.tsx
@@ -24,31 +24,64 @@ const WhyINFPage: React.FC<Props> = ({ data, location }: Props) => {
       <Meta site={meta} title="Why INF - INF Visitor Insurance" />
       <BackgroundImage
         Tag="section"
-        className="hero-section-bg"
+        className="why-inf-hero-section-bg"
         fluid={hero_background}
         backgroundColor={`#e9f4f8`}
-        alt=""
+        alt="why inf hero background"
       >
-        <div id="hero-section">
+        <div id="why-inf-hero-section">
           <Container>
             <div className="col-lg-12">
-              <h1 className="text-black">
-                Why INF page
+              <h1 className="text-white text-center">
+                WHY INF?
               </h1>
-              <p className="hero-text text-black">
-                subtitle here
-              </p>
-              <ButtonBlack path="/contact" label="Button Text" />
-            </div>
-            <div className="col-lg-8">
+              <button className="hero-btn"> Get a Quote </button>
             </div>
           </Container>
         </div>
       </BackgroundImage>
-      <div id="your-id-here">
+
+      <section id="why-inf-section-1">
         <Container>
+          <div className="col-lg-12">
+            <div className="why-inf-container">
+              <h2 className="text-blue text-center">Why choose INF Insurance ?</h2>
+              <p className="title">We Really do Care about Our Members</p>
+              <p>Since our founding in 1988, INF has provided innovative Insurance for Visitors options for members in the community. At INF- we really do care about our members. Our dedicated staff answers your calls and emails within 24 hours.</p>
+              <p>Each plan participant is assigned an account manager, who will help the insured with any of their needs. Our account managers provide you with their direct WhatsApp numbers so you have someone to talk if an emergency happens during their travel.</p>
+              <p>At INF, we don't give you the runaround. You can talk to the same person each time you call our office, reach the same person whenever you have a question. We are your trusted advisor when it comes to your Insurance for Visitors, and we do not take that responsibility lightly.</p>
+
+              <p className="title">Built in Technology & With Simplicity</p>
+              <p>INF was molded with technology & simplicity in mind. Our easy to use site simplifies the shopping process, and provide unprecedented access to our customer support team of licensed insurance agents.</p>
+              <p>Manage your policy easily using the MyINF portal. MyINF allows you to track your purchase history, Extend or apply for new coverage, manage claims, and much more! </p>
+
+              <p className="title">Our 30 year Track Record</p>
+              <p>INF has offered innovative and cost effective insurance options since 1988. Our commitment to you or your families health remains paramount in our mission to provide the best insurance options for Non-US persons traveling to the US, Canada or Mexico.</p>
+
+              <p className="title">Expertise in Visitors Insurance</p>
+              <p>Our veteran customer support team has over 200 years of combined experience in the international visitor accident & sickness insurance field. We will provide superior service to make sure you and your loved ones needs are met.</p>
+
+              <p className="title">24 hour Customer Service</p>
+              <p>We will answer your emergency requirements at any time of day. If you call us outside of normal operating hours and leave a message, our staff will return your phone call within a few hours. </p>
+
+              <p className="title">Full Transparency</p>
+              <p>We have each plan's documents and brochures readily available and easy to read. We do not have anything to hide and we will be the most transparent, trusted advisor for you and your family. </p>
+
+              <p className="title">The Company with Patents</p>
+              <p>We've heavily invested in technology to make your life easier. Our MyINF Web Portal & Quote engine are patent pending. We are dedicated to making insurance the most transparent purchase you will ever make.</p>
+
+              <p className="title">Instant ID. Secure Purchase.</p>
+              <p>Our site uses the most advanced encryption technology to protect your data & ensure you and your family never have to worry about a transaction gone wrong. Applications can be completed in less than 5 minutes! We do not ask for any medical paperwork or immigration documents.</p>
+
+              <p className="title">Unique Plan Offerings</p>
+              <p>We offer plans exclusively found on this platform. Some of our plans cover pre-existing conditions and membership provides several non-insurance benefits such as Dental, Vision, Hearing, and Prescription Discounts coordinated by Careington International, and MeMD Telehealth (which are both only available in the US)</p>
+              <p>These non-insurance benefits are not affiliated with Crum & Forster, SPC </p>
+
+            </div>
+          </div>
         </Container>
-      </div>
+      </section>
+
     </Layout>
   )
 }
@@ -64,7 +97,7 @@ export const query = graphql`
         siteUrl
       }
     },
-    hero_background: file(name: { eq: "bg" }) {
+    hero_background: file(name: { eq: "why-inf-hero-bg" }) {
       childImageSharp {
         fluid(maxWidth: 1400, quality: 100) {
           ...GatsbyImageSharpFluid
