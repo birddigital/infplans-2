5268ebd167edfd08861c4310a6033dabd4d6dbf2
Jomar <jomarbruto@gmail.com>
2021-07-28 21:48:38 +0800

    ---

--- src/components/forms/GetAQuote.tsx
+++ src/components/forms/GetAQuote.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useLayoutEffect, useEffect, useState } from "react";
 import { Link } from 'gatsby'
 import Container from 'components/pages/container'
 import IframeResizer from 'iframe-resizer-react'
@@ -8,10 +8,30 @@ interface Props {
 }
 
 const GetAQuote: React.FC<Props> = ({ path }: Props) => {
+  /*const targetRef = useRef();
+  const [dimensions, setDimensions] = useState({ height: 0 });
+  const [count, setCount] = useState(1);
+
+  useLayoutEffect(() => {
+    if (targetRef.current) {
+      setDimensions({
+        width: targetRef.current.offsetWidth,
+        height: targetRef.current.offsetHeight
+      });
+    }
+  }, [count]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(count + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);*/
+
   return (
     <section className="pb-0">
       <Container>
-        <div className="col-lg-12" id="quote-form-container">
+        <div className="col-lg-12" id="quote-form-container" >
           <IframeResizer
             log
             src="https://adrienb3.sg-host.com/forms/get-a-quote.php?agentno=1000000"
